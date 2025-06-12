2aa0f4c0f4c8f000c92177ef406bb22ce6162117
ccfelius <felius@cwi.nl>
2025-06-12 22:36:43 +0200

    replacing db files for smaller ones

--- tools/shell/tests/test_encryption.py
+++ tools/shell/tests/test_encryption.py
@@ -6,9 +6,9 @@ from conftest import ShellTest
 import os
 
 # the correct keys used are:
-# 'masterkey' for created_with_master_key.db
-# 'userkey' for created_with_user_key.db
-# 'attachkey' for created_with_attach.db
+# 'masterkey' for encrypted_master_key.db
+# 'userkey' for encrypted_key.db
+# 'attachkey' for encrypted_attach.db
 
 
 def test_no_key(shell):
@@ -17,15 +17,15 @@ def test_no_key(shell):
     throws an error
     """
 
-    test = ShellTest(shell, arguments=['test/storage/encryption/created_with_master_key.db'])
+    test = ShellTest(shell, arguments=['test/storage/encryption/encrypted_mkey.db'])
 
     result = test.run()
     result.check_stderr(
-        'Error: unable to open database "test/storage/encryption/created_with_master_key.db": Catalog Error: Cannot open encrypted database "test/storage/encryption/created_with_master_key.db" without a key'
+        'Error: unable to open database "test/storage/encryption/encrypted_mkey.db": Catalog Error: Cannot open encrypted database "test/storage/encryption/encrypted_mkey.db" without a key'
     )
 
 
-def test_unencrypted_database_user_key(shell):
+def test_unencrypted_database_key(shell):
     """
     Unencrypted database opened with -key
     throws an error
@@ -56,14 +56,14 @@ def test_unencrypted_database_master_key(shell):
 def test_correct_master_key(shell):
     """
     Encrypted database created with -master_key in the command line
-    Opened with 'created_with_user_key.db' -master_key masterkey
+    Opened with 'encrypted_key.db' -master_key masterkey
     """
 
     test = (
         ShellTest(shell, arguments=['-master_key', 'masterkey'])
-        .statement("ATTACH 'test/storage/encryption/created_with_master_key.db' as enc")
+        .statement("ATTACH 'test/storage/encryption/encrypted_mkey.db' as enc")
         .statement("USE enc")
-        .statement("select l_orderkey from lineitem limit 1;")
+        .statement("select * from tbl;")
     )
 
     result = test.run()
@@ -73,12 +73,12 @@ def test_correct_master_key(shell):
 def test_wrong_master_key(shell):
     """
     Encrypted database created with -master_key in the command line
-    Opened with 'created_with_user_key.db' -master_key masterkey
+    Opened with 'encrypted_key.db' -master_key masterkey
     Incorrect master key is given as input
     """
 
     test = ShellTest(shell, arguments=['-master_key', 'xxx']).statement(
-        "ATTACH 'test/storage/encryption/created_with_master_key.db' as enc"
+        "ATTACH 'test/storage/encryption/encrypted_mkey.db' as enc"
     )
 
     result = test.run()
@@ -87,52 +87,52 @@ def test_wrong_master_key(shell):
     )
 
 
-def test_correct_user_key(shell):
+def test_correct_key(shell):
     """
     Encrypted database created with -key in the command line
-    Opened with 'created_with_user_key.db' -key userkey
+    Opened with 'encrypted_key.db' -key userkey
     """
-    test = ShellTest(
-        shell, arguments=['test/storage/encryption/created_with_user_key.db', '-key', 'userkey']
-    ).statement("select l_orderkey from lineitem limit 1;")
+    test = ShellTest(shell, arguments=['test/storage/encryption/encrypted_key.db', '-key', 'userkey']).statement(
+        "select * from tbl;"
+    )
 
     result = test.run()
     result.check_stdout('1')
 
 
-def test_wrong_user_key(shell):
+def test_wrong_key(shell):
     """
     Encrypted database created with -key in the command line
-    Opened with 'created_with_user_key.db' -key
+    Opened with 'encrypted_key.db' -key
     """
 
-    test = ShellTest(shell, arguments=['test/storage/encryption/created_with_user_key.db', '-key', 'xxx'])
+    test = ShellTest(shell, arguments=['test/storage/encryption/encrypted_key.db', '-key', 'xxx'])
 
     result = test.run()
     result.check_stderr('IO Error: Wrong encryption key used to open the database file')
 
 
-def test_correct_user_key_attach(shell):
+def test_correct_key_attach(shell):
     """
     Encrypted database created with attach
-    Opened with 'created_with_attach.db' -key
+    Opened with 'encrypted_attach.db' -key
     """
 
-    test = ShellTest(
-        shell, arguments=['test/storage/encryption/created_with_attach.db', '-key', 'attachkey']
-    ).statement("select l_orderkey from lineitem limit 1;")
+    test = ShellTest(shell, arguments=['test/storage/encryption/encrypted_attach.db', '-key', 'attachkey']).statement(
+        "select * from tbl;"
+    )
 
     result = test.run()
     result.check_stdout('1')
 
 
-def test_wrong_user_key_attach(shell):
+def test_wrong_key_attach(shell):
     """
     Encrypted database created with attach
-    Opened with 'created_with_attach.db' -key
+    Opened with 'encrypted_attach.db' -key
     """
 
-    test = ShellTest(shell, arguments=['test/storage/encryption/created_with_attach.db', '-key', 'xxx'])
+    test = ShellTest(shell, arguments=['test/storage/encryption/encrypted_attach.db', '-key', 'xxx'])
 
     result = test.run()
     result.check_stderr('IO Error: Wrong encryption key used to open the database file')
@@ -146,7 +146,7 @@ def test_wrong_master_key_no_explicit_attach_key(shell):
     """
 
     test = ShellTest(shell, arguments=['-master_key', 'masterkey']).statement(
-        "ATTACH 'test/storage/encryption/created_with_attach.db' as enc"
+        "ATTACH 'test/storage/encryption/encrypted_attach.db' as enc"
     )
 
     result = test.run()
@@ -158,39 +158,39 @@ def test_wrong_master_key_no_explicit_attach_key(shell):
 def test_wrong_master_key_correct_attach(shell):
     """
     Encrypted database created with attach
-    Opened with 'created_with_attach.db' -master_key
+    Opened with 'encrypted_attach.db' -master_key
     but explicitly uses another key on attach
     """
 
     test = (
         ShellTest(shell, arguments=['-master_key', 'masterkey'])
-        .statement("ATTACH 'test/storage/encryption/created_with_attach.db' as enc (ENCRYPTION_KEY attachkey)")
+        .statement("ATTACH 'test/storage/encryption/encrypted_attach.db' as enc (ENCRYPTION_KEY attachkey)")
         .statement("USE enc")
-        .statement("select l_orderkey from lineitem limit 1;")
+        .statement("select * from tbl;")
     )
 
     result = test.run()
     result.check_stdout('1')
 
 
-def test_explicit_user_key_and_master_key(shell):
+def test_explicit_key_and_master_key(shell):
     """
-    Encrypted database created with user_key
-    Opened with 'created_with_user_key.db' -master_key masterkey -user_key userkey
+    Encrypted database created with key
+    Opened with 'encrypted_key.db' -master_key masterkey -key userkey
     """
 
     test = ShellTest(
         shell,
-        arguments=['-master_key', 'masterkey', '-key', 'userkey', 'test/storage/encryption/created_with_user_key.db'],
-    ).statement("select l_orderkey from lineitem limit 1;")
+        arguments=['-master_key', 'masterkey', '-key', 'userkey', 'test/storage/encryption/encrypted_key.db'],
+    ).statement("select * from tbl;")
 
     result = test.run()
     result.check_stderr(
-        'Cannot specify both -key and -master_key.\nError: unable to open database "test/storage/encryption/created_with_user_key.db'
+        'Cannot specify both -key and -master_key.\nError: unable to open database "test/storage/encryption/encrypted_key.db'
     )
 
 
-def test_user_key_no_database(shell):
+def test_key_no_database(shell):
     """
     Opened with -key userkey
     but no database is found as input
@@ -202,11 +202,7 @@ def test_user_key_no_database(shell):
     result.check_stderr('Error: key specified but no database found')
 
 
-# Long test key: test_key_U2FsdGVkX18+0aLSpbJ9V0hVTVV1oYlJbVhGZ0NMTkpqaWp4R3hNR2VaUkVHaVRqR1htR3V1QXZmc0NtbUxSaUlwWVdoVkNrT0RUR2pqUGxkUVdtSmtOUldHWlJRa05JUWlVSkZaVk10aEZXR2FRa0Z6eEx0VlZIVG9LR0UwRkdNZ0xXaEpKWkNKRW5BaXlLSnNWTkdWTkpHZ0ZR
-#
-
-
-def test_long_base64_user_key(shell):
+def test_long_base64_key(shell):
     """
     Opened with a long -key
     """
@@ -214,11 +210,11 @@ def test_long_base64_user_key(shell):
     test = ShellTest(
         shell,
         arguments=[
-            'test/storage/encryption/base64_encrypted.db',
+            'test/storage/encryption/encrypted_long_key.db',
             '-key',
             'test_key_U2FsdGVkX18+0aLSpbJ9V0hVTVV1oYlJbVhGZ0NMTkpqaWp4R3hNR2VaUkVHaVRqR1htR3V1QXZmc0NtbUxSaUlwWVdoVkNrT0RUR2pqUGxkUVdtSmtOUldHWlJRa05JUWlVSkZaVk10aEZXR2FRa0Z6eEx0VlZIVG9LR0UwRkdNZ0xXaEpKWkNKRW5BaXlLSnNWTkdWTkpHZ0ZR',
         ],
-    ).statement("select l_orderkey from lineitem limit 1;")
+    ).statement("select * from tbl;")
 
     result = test.run()
     result.check_stdout('1')
@@ -237,9 +233,9 @@ def test_long_base64_master_key(shell):
                 'test_key_U2FsdGVkX18+0aLSpbJ9V0hVTVV1oYlJbVhGZ0NMTkpqaWp4R3hNR2VaUkVHaVRqR1htR3V1QXZmc0NtbUxSaUlwWVdoVkNrT0RUR2pqUGxkUVdtSmtOUldHWlJRa05JUWlVSkZaVk10aEZXR2FRa0Z6eEx0VlZIVG9LR0UwRkdNZ0xXaEpKWkNKRW5BaXlLSnNWTkdWTkpHZ0ZR',
             ],
         )
-        .statement("ATTACH 'test/storage/encryption/base64_encrypted.db' as enc")
+        .statement("ATTACH 'test/storage/encryption/encrypted_long_key.db' as enc")
         .statement("USE enc")
-        .statement("select l_orderkey from lineitem limit 1;")
+        .statement("select * from tbl;")
     )
 
     result = test.run()
